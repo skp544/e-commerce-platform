1bb7f99833228f5bfe6638e86e4b865272c25fc7
skp544 <shubhamkumarprajapati544@gmail.com>
2025-05-11 01:34:09 +0530

    adding theme and shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import ThemeToggle from "@/components/common/theme-toggle";
 
 export default function Home() {
-  return <div className={"font-bold text-blue-500"}>Home</div>;
+  return (
+    <div className={"p-5"}>
+      <div className={"flex w-full justify-end"}>
+        <ThemeToggle />
+      </div>
+      <h1 className={"font-barlow font-bold text-blue-500"}>Home</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
 }
